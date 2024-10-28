1541fe256a8259b27f0a54d3992ae043c463972f
user.name <user.email>
2024-10-28 17:17:52 +0200

    US1: Test game adding and getting 10 frames

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -22,3 +22,22 @@ class TestBowlingGame(unittest.TestCase):
         game = BowlingGame()
         game.add_frame(frame)
         self.assertEqual(frame, game.get_frame_at(0))
+
+    def test_add_get_10_frames(self):
+        frames = [
+            [1, 5],
+            [3, 6],
+            [7, 2],
+            [3, 6],
+            [4, 4],
+            [5, 3],
+            [3, 3],
+            [4, 5],
+            [8, 1],
+            [2, 6]
+        ]
+        game = BowlingGame()
+        for idx, value in enumerate(frames):
+            frame = Frame(value[0], value[1])
+            game.add_frame(frame)
+            self.assertEqual(frame, game.get_frame_at(idx))
